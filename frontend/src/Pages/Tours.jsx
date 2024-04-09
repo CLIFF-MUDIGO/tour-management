c1dad862ea3dcea2a55d7cd7b9d4e6da04450db3
cliff <cliffmudigo5@gmail.com>
2024-04-09 10:22:42 +0300

    :construction: work in progress

--- frontend/src/Pages/Tours.jsx
+++ frontend/src/Pages/Tours.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState , useEffect} from 'react';
 import CommonSection from '../shared/CommonSection';
 import '../styles/tour.css';
 
@@ -9,6 +9,12 @@ import Newsletter from './../shared/Newsletter';
 import { Container, Row, Col } from 'reactstrap';
 
 const Tours = () => {
+  const [pageCount, setPageCount] = useState(0);
+const [page, setPage] = useState(0); 
+useEffect(() => {
+  const pages = Math.ceil(5 / 4); //replace it with backend data count
+  setPageCount(pages);
+}, [page]); 
   return (
     <>
       <CommonSection title={"All Tours"}/>
@@ -30,6 +36,19 @@ const Tours = () => {
               </Col>
             ))}
             <Col lg='12'>
+              <div className='pagination d-flex align-items-center justify-content-center gap-3 mt-4'>
+                {[...Array(pageCount).keys()].map((number) => (
+                  <span
+                    key={number}
+                    onClick={() => setPage(number)}
+                    className={page === number ? 'active__page' : ''}
+                    
+                  >
+                    {number + 1}
+                  </span>
+                ))}
+
+              </div>
             </Col>
           </Row>
         </Container>
