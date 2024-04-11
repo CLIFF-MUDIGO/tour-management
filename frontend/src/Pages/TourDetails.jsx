6756932a0bd2b8ed28a20141bdcdb48b77700f7e
cliff <cliffmudigo5@gmail.com>
2024-04-11 08:53:17 +0300

    :boom: introduce breaking changes

--- frontend/src/Pages/TourDetails.jsx
+++ frontend/src/Pages/TourDetails.jsx
@@ -1,11 +1,40 @@
 import React from 'react'
+import '../styles/tour-details.css'
+import { Container, Row, Col, Form, ListGroup } from 'reactstrap'
+import {useParams} from "react-router-dom"
+import tourData from '../assets/data/tours'
 
 const TourDetails = () => {
-  return (
-    <div>
-      tourdetails
-    </div>
-  )
+  
+const {id} = useParams()
+
+//this is an static data later we will call our API and load our data from database
+const tour = tourData.find(tour => tour.id === id)
+
+//destructure properties from tour project
+const {photo, title, desc, city, distance, price, reviews,  maxGroupSize} = tour
+
+
+  return <>
+  
+  <section>
+    <Container>
+      <Row>
+        <Col lg='8'>
+          <div className="tour__content">
+            <img src={photo} alt="" />
+            <div className="tour__info">
+            <h2>{title}</h2>
+            </div>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  </section>
+  
+  
+  
+  </>
 }
 
 export default TourDetails
--- frontend/src/Pages/Tours.jsx
+++ frontend/src/Pages/Tours.jsx
@@ -54,7 +54,7 @@ useEffect(() => {
         </Container>
       </section>
       
-     
+     <Newsletter/>
 
     </>
   );
